750a8844d5c9d93e1a2fc9f41aa8d5a5bae972eb
mmms2000 <moseskmcmmms@gmail.com>
2025-11-10 00:45:07 +0900

    feat(theme): sync browser meta theme-color with dark/light mode/ chaged to useEffect

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -9,9 +9,16 @@ export const ThemeToggle = () => {
     const storedTheme = localStorage.getItem("theme");
     if (storedTheme === "dark") {
       setIsDarkMode(true);
+
+      const metaThemeColor = document.querySelector("meta[name='theme-color']");
+    if (metaThemeColor) metaThemeColor.setAttribute("content", "#ffffff");
+    
       document.documentElement.classList.add("dark");
     } else {
       setIsDarkMode(false);
+      const metaThemeColor = document.querySelector("meta[name='theme-color']");
+    if (metaThemeColor) metaThemeColor.setAttribute("content", "#0f172a");
+
       document.documentElement.classList.remove("dark");
     }
   }, []);
@@ -21,6 +28,7 @@ export const ThemeToggle = () => {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("theme", "light");
       setIsDarkMode(false);
+
     } else {
       document.documentElement.classList.add("dark");
       localStorage.setItem("theme", "dark");
