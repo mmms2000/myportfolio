a8e2b159c5c34b79abc77fdf5d7090a5d0cf5461
mmms2000 <moseskmcmmms@gmail.com>
2025-11-16 19:59:59 +0900

    Feat: isScrolled is hidden when it is scroll

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,11 @@
 import { ArrowDown } from "lucide-react";
+import { cn } from "../lib/utils";
+import { useScroll } from "../lib/useScroll";
+
 
 export const HeroSection = () => {
+
+    const {isScrolled} = useScroll(10);
   return (
     <section
       id="hero"
@@ -32,7 +37,9 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce"> 
+      <div className={cn("absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce",
+        isScrolled? "hidden" :"",
+      )}> 
       <span className="text-sm text-muted-foreground mb-2"> Scroll</span>
       <ArrowDown className="h-5 w-5 text-primary"/>
       </div>
