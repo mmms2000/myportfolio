6825167f5f06b51f2749fc2ec76e45e8df9c88aa
mmms2000 <moseskmcmmms@gmail.com>
2025-11-25 15:36:02 +0900

    add vercel analysics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />}/>
-          <Route path="*" element={<NotFound />}/>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
           <Route />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
